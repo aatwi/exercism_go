f58c6cc5438536210fba9e629d2d42a3e6a4f914
aatwi <ahmadatwi23@gmail.com>
2024-08-25 18:24:23 +0200

    add the vehicle-purchase problem

---
+++ vehicle-purchase/vehicle_purchase.go
@@ -0,0 +1,46 @@
+package purchase
+
+// NeedsLicense determines whether a license is needed to drive a type of vehicle. Only "car" and "truck" require a license.
+func NeedsLicense(kind string) bool {
+	if kind == "car" {
+		return true
+	}
+	if kind == "truck" {
+		return true
+	}
+	if kind == "bike" {
+		return false
+	}
+	if kind == "stroller" {
+		return false
+	}
+	if kind == "e-scooter" {
+		return false
+	}
+	panic("NeedsLicense not implemented")
+}
+
+// ChooseVehicle recommends a vehicle for selection. It always recommends the vehicle that comes first in lexicographical order.
+func ChooseVehicle(option1, option2 string) string {
+	if option1 < option2 {
+		return option1 + " is clearly the better choice."
+	}
+	if option2 < option1 {
+		return option2 + " is clearly the better choice."
+	}
+	panic("ChooseVehicle not implemented")
+}
+
+// CalculateResellPrice calculates how much a vehicle can resell for at a certain age.
+func CalculateResellPrice(originalPrice, age float64) float64 {
+	if age < 3 {
+		return originalPrice * 0.8
+	}
+	if age >= 3 && age < 10 {
+		return originalPrice * 0.7
+	}
+	if age >= 10 {
+		return originalPrice * 0.5
+	}
+	panic("CalculateResellPrice not implemented")
+}
