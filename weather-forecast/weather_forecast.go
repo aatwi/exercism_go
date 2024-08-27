10fd7cf815bf7e0aaac81cf8bacaf7014269bbf7
Ahmad ATWI <ahmadatwi23@gmail.com>
2024-08-27 15:16:57 +0200

    Update WeatherForecast

--- weather-forecast/weather_forecast.go
+++ weather-forecast/weather_forecast.go
@@ -1,12 +1,13 @@
-/*
-	Package weather has 2 variables and one function
-*/
-
+// Package weather is a class that holds weather information.
 package weather
 
+// CurrentCondition is the current value of the weather condition.
 var CurrentCondition string
+
+// CurrentLocation is the current value of the location.
 var CurrentLocation string
 
+// Forecast is a function that returns a string based on the city and condition passed by the user.
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
