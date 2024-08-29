052f6243a3ea3ba94c2cec91568a38bffff80659
Ahmad ATWI <ahmadatwi23@gmail.com>
2024-08-29 16:44:05 +0200

    Add solution for CollatzConjecture

--- exercises/collatzconjecture/collatzconjecture.go
+++ exercises/collatzconjecture/collatzconjecture.go
@@ -1,5 +1,24 @@
 package collatzconjecture
 
+import "errors"
+
 func CollatzConjecture(n int) (int, error) {
-	panic("Please implement the CollatzConjecture function")
+	if n <= 0 {
+		return 0, errors.New("Invalid Number")
+	}
+	return collatzConjecture(n), nil
+}
+
+func collatzConjecture(n int) int {
+	if n == 1 {
+		return 0
+	}
+	if isEven(n) {
+		return 1 + collatzConjecture(n/2)
+	}
+	return 1 + collatzConjecture(3*n+1)
+}
+
+func isEven(n int) bool {
+	return n%2 == 0
 }
